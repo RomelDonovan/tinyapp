d8c77287b4e43913d0b2976962956ae186418c58
RomelDonovan <RomelDonovan@live.ca>
2023-09-05 02:33:36 +0000

    Add render urls_new

--- express_server.js
+++ express_server.js
@@ -35,6 +35,10 @@ app.get("/set", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const templateVars = { id: id, longURL: urlDatabase[id] };
